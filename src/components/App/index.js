5acb4c2c59f5d3b00d0b6342a617a71f125dff7f
Michael <michaelchapa1@gmail.com>
2019-12-23 16:17:44 -0600

    Added Router, Routes, and Navigation to the App/index.js file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navigation from "../Navigation";
+import LandingPage from "../Landing";
+import SignUpPage from "../SignUp";
+import SignInPage from "../SignIn";
+import PasswordForgetPage from "../PasswordForget";
+import HomePage from "../Home";
+import AccountPage from "../Account";
+import AdminPage from "../Admin";
+
+import * as ROUTES from "../../constants/routes.js";
 
 const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+  /* Router makes it possible to navigate from URL-to-URL on the client-side application 
+  without another request to a web server for every route change. */
+  <Router>
+    <React.Fragment>
+      <Navigation />
+
+      <hr />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </React.Fragment>
+  </Router>
 );
 
 export default App;
